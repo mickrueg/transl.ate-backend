a185133ff4920dfeab98fdd2d6e382a01e88e1b8
mickrueg <mickrueg@gmail.com>
2023-03-05 08:57:40 -0700

    Passing in date to input schema

--- Input.js
+++ Input.js
@@ -1,7 +1,8 @@
 const mongoose = require('./connection');
 
 const inputSchema = new mongoose.Schema({
-    userInput: String
+    userInput: String,
+    timestamp: Date
 })
 
 //The model name in between the ' ' below, has to be the name of the database in MongoDB. Letters must all be lowercase and MongoDB will automatically pluralize.
